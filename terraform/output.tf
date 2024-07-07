6c524d5e54a94d1f26236b0d9820fc1dd3c679ba
Gonzalo Mahserdjian <gsmx64@outlook.com>
2024-07-07 14:43:37 -0300

    Mod: Terraform added output

--- terraform/output.tf
+++ terraform/output.tf
@@ -5,6 +5,6 @@ output "main_ip" {
 output "k8s_cluster_info" {
   value = {
     master_ip   = google_compute_instance.master.network_interface[0].access_config[0].nat_ip
-    worker_ips  = google_container_node_pool.k8s_workers.*.instance_group_urls
+    workers_ips = google_compute_instance.workers.network_interface[0].access_config[0].nat_ip
   }
 }
